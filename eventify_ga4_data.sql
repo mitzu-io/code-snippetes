7172112c3b9a284ad07cbe11fa758d8fc8ac28a2
Istvan Meszaros <IstvanM@users.noreply.github.com>
2024-04-03 11:09:18 +0200

    Create eventify_ga4_data.sql

---
+++ eventify_ga4_data.sql
@@ -0,0 +1,60 @@
+CREATE OR REPLACE TABLE
+  analytics_307711707.ga4_events 
+AS (
+WITH
+  event_properties AS (
+  SELECT
+    event_timestamp,
+    user_id,
+    user_pseudo_id,
+    event_name,
+    PARSE_JSON("{\""|| STRING_AGG(p.key || "\":\""|| COALESCE(coalesce(COALESCE(p.value.string_value, CAST(p.value.int_value AS string)),
+            CAST(p.value.float_value AS string)), CAST(p.value.double_value AS string)),"\",\"") || "\"}") AS event_params,
+  FROM
+    `mitzu-358611.analytics_307711707.events_*`,
+    UNNEST(event_params) AS p
+  GROUP BY
+    1,
+    2,
+    3,
+    4 ),
+  user_properties AS (
+  SELECT
+    event_timestamp,
+    user_id,
+    user_pseudo_id,
+    event_name,
+    PARSE_JSON("{\""|| STRING_AGG(p.key || "\":\""|| COALESCE(coalesce(COALESCE(p.value.string_value, CAST(p.value.int_value AS string)),
+            CAST(p.value.float_value AS string)), CAST(p.value.double_value AS string)),"\",\"") || "\"}") AS user_properties,
+  FROM
+    `mitzu-358611.analytics_307711707.events_*`,
+    UNNEST(user_properties) AS p
+  GROUP BY
+    1,
+    2,
+    3,
+    4 )
+SELECT
+  TIMESTAMP_MICROS(evt.event_timestamp) AS event_time,
+  ep.event_params,
+  up.user_properties,
+  evt.* EXCEPT (event_timestamp,
+    event_params,
+    user_properties,
+    items)
+FROM
+  `mitzu-358611.analytics_307711707.events_*` AS evt
+LEFT JOIN
+  event_properties ep
+ON
+  evt.event_timestamp = ep.event_timestamp
+  AND COALESCE(evt.user_id,'')=COALESCE(ep.user_id,'')
+  AND evt.user_pseudo_id =ep.user_pseudo_id
+  AND evt.event_name = ep.event_name
+LEFT JOIN
+  user_properties up
+ON
+  evt.event_timestamp = up.event_timestamp
+  AND COALESCE(evt.user_id,'')=COALESCE(up.user_id,'')
+  AND evt.user_pseudo_id =up.user_pseudo_id
+  AND evt.event_name = up.event_name)
